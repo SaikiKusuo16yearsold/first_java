5fe09612f4c18c994e45d2e52e0f026337444211
имя фамилия <Dima>
2024-09-30 23:17:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int maria = 67760;
+        int denis = 83690;
+        int cristina = 76230;
+        float newPayForMaria =  (maria * 1.1f);
+        float newPayForDenis =  (denis * 1.1f);
+        float newPayForCristina =  (cristina * 1.1f);
+        float differenceInSalaryMaria =  newPayForMaria - maria;
+        float differenceInSalaryDenis =  newPayForDenis - denis;
+        float differenceInSalaryCristina =  newPayForCristina - cristina;
+        System.out.println("Маша теперь получает " + newPayForMaria + " рублей. Годовой доход вырос на " + differenceInSalaryMaria +  " рублей");
+        System.out.println("Денис теперь получает " + newPayForDenis + " рублей. Годовой доход вырос на " + differenceInSalaryDenis +  " рублей");
+        System.out.println("Кристина теперь получает " + newPayForCristina + " рублей. Годовой доход вырос на " + differenceInSalaryCristina +  " рублей");
+
+    }
+}
